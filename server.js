4f2f962280f773245a11de4cafef5787dd6e87a3
katcozadd <krosecozadd@gmail.com>
2018-06-07 17:26:07 -0600

    creating sendgrid functionality

---
+++ server.js
@@ -0,0 +1,39 @@
+const express    = require('express');
+const app        = express();
+const path       = require('path');
+const bodyParser = require('body-parser');
+const sgMail     = require('@sendgrid/mail');
+const cors       = require('cors')
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  next();
+});
+
+app.post('/contact', function(req, res) {
+	console.log(req.body);
+			sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+				const msg = {
+				  to: 'krosecozadd@gmail.com',
+				  from: `${req.body.email}`,
+				  subject: 'New contact submission from leafvegetarianrestaurant.com: ' + `${req.body.subject}`,
+				  text: `${req.body.message}`,
+				  html: `${req.body.message}`,
+				};
+				sgMail.send(msg);
+				res.json(msg);
+	});
+
+
+// listen on port 3000
+  app.set('port', process.env.PORT || 3001)
+
+  app.listen(app.get('port'), () => {
+    console.log(`✅ PORT: ${app.get('port')} 🌟`)
+  })
